d5f37bf037f48c0e30e77ec29bc24e2c45025647
Anatolii <116556949+Anoonimik@users.noreply.github.com>
2024-12-11 23:33:50 +0100

    Update Footer.tsx

--- src/components/home-page/Footer.tsx
+++ src/components/home-page/Footer.tsx
@@ -10,7 +10,6 @@ import Typography from "@mui/material/Typography";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import SitemarkIcon from "./SitemarkIcon.tsx";
 
 function Copyright() {
   return (
